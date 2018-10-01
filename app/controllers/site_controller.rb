05af058e93181aebc60ea044bc408ea367c4f34c
sonu-pajai <sonupajai22@gmail.com>
2018-10-01 15:28:49 +0530

    code indentation

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,17 +1,17 @@
 class SiteController < ApplicationController
-	layout 'site'
+  layout 'site'
 
-	def index
-		@photos= Photo.order("created_at desc").limit(25)
-	end
+  def index
+    @photos= Photo.order("created_at desc").limit(25)
+  end
 
-	def userprofile
-		@user=User.find_by_id(params[:id])
-		unless @user
-			logger.warn "User with id #{params[:id]} does not exists"
-		end
-		@albums = @user.albums.paginate(:page => params[:page], :per_page => 10)
-	end
+  def userprofile
+    @user=User.find_by_id(params[:id])
+    unless @user
+      logger.warn "User with id #{params[:id]} does not exists"
+    end
+    @albums = @user.albums.paginate(:page => params[:page], :per_page => 10)
+  end
 
 
 end
